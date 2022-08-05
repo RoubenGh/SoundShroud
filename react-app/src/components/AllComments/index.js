f85f34f252a372a955b43f52701bbcd8aba607f3
Rouben <rouben.ghambaryan@gmail.com>
2022-08-04 21:46:51 -0700

    get all comments and add comments done

--- react-app/src/components/AllComments/index.js
+++ react-app/src/components/AllComments/index.js
@@ -10,9 +10,12 @@ function AllComments() {
 	const { id } = useParams();
 
 	const oneSong = useSelector((state) => state.songs[id]);
-	console.log(oneSong, 'this is my one song --------');
+	// console.log(oneSong, 'this is my one song --------');
 	const user = useSelector((state) => state.session.user);
-    const content = useSelector((state) => state.comments);
+    const commentObject = useSelector((state) => state.comments);
+
+    const singleComment = Object.values(commentObject)
+    console.log(singleComment, '922222222222222222222222')
 
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [comment, setComment] = useState('');
@@ -23,51 +26,50 @@ function AllComments() {
 		setIsLoaded(true);
 	}, [dispatch, id]);
 
-	// const notLoggedIn = () => {
-	// 	if (!user) history.push('/login');
-	// };
+	const notLoggedIn = () => {
+		if (!user) history.push('/login');
+	};
 
-	// const handleSubmit = async (e) => {
-	// 	e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-	// 	const data = {
-	// 		user_id: user.id,
-	// 		song_id: oneSong.id,
-	// 		content,
-	// 		username: user.username,
-	// 	};
+		const data = {
+			user_id: user.id,
+			song_id: oneSong.id,
+			content: comment,
+			username: user.username,
+		};
 
-	// 	await dispatch(addCommentBySongId(data));
-	// 	setComment('');
-	// };
+		await dispatch(addCommentBySongId(data));
+		setComment('');
+	};
 
-	// if (!user) return <Redirect to="/login" />;
+	if (!user) return <Redirect to="/login" />;
 
 	return (
-        <div>COMMENTS</div>
-		// isLoaded && (
-		// 	<div>
-		// 		<div>
-		// 			<form onSubmit={handleSubmit}>
-		// 				<input
-		// 					type="text"
-		// 					value={comment}
-		// 					onChange={(e) => setComment(e.target.value)}
-		// 					placeholder="Add a comment"
-		// 				/>
-		// 				<button type="submit">Submit</button>
-		// 			</form>
-		// 		</div>
-		// 		<div>
-		// 			{oneSong.comments.map((comment) => (
-		// 				<div key={comment.id}>
-		// 					<h1>{comment.content}</h1>
-		// 					<h2>{comment.username}</h2>
-		// 				</div>
-		// 			))}
-		// 		</div>
-		// 	</div>
-		// )
+		isLoaded && (
+			<div>
+				<div>
+					<form onSubmit={handleSubmit}>
+						<input
+							type="text"
+							value={comment}
+							onChange={(e) => setComment(e.target.value)}
+							placeholder="Add a comment"
+						/>
+						<button type="submit">Submit</button>
+					</form>
+				</div>
+				<div>
+					{singleComment.map((comment) => (
+						<div key={comment.id}>
+							<p>{comment.content}</p>
+							<p>{comment.username}</p>
+						</div>
+					))}
+				</div>
+			</div>
+		)
         )
     }
 
