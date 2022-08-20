6e79ff1277e4818f704e593f8895fec41c0d588b
Rouben <rouben.ghambaryan@gmail.com>
2022-08-20 15:23:22 -0700

    fixed bug

--- react-app/src/components/User/User.js
+++ react-app/src/components/User/User.js
@@ -1,45 +1,86 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, NavLink, Redirect } from 'react-router-dom';
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
 import { getAllSongs } from '../../store/songs';
-import './User.css'
+import './User.css';
 import { getSingleUser } from '../../store/user';
 
 function User() {
-	const [user, setUser] = useState({});
+	const dispatch = useDispatch();
+	const sessionUser = useSelector((state) => state.session.user);
+	// const [user, setUser] = useState({});
 	const { userId } = useParams();
+	const usersObject = useSelector((state) => state.user);
+	let user;
+	if (usersObject) {
+		user = usersObject[userId];
+	}
+
+	const songsObject = useSelector((state) => state.songs);
+	const songs = Object.values(songsObject);
 
 	useEffect(() => {
-		if (!userId) {
-			return;
-		}
-		(async () => {
-			const response = await fetch(`/api/users/${userId}`);
-			const user = await response.json();
-			setUser(user);
-		})();
-	}, [userId]);
+		dispatch(getSingleUser(userId));
+		dispatch(getAllSongs());
+	}, [dispatch, userId]);
 
 	if (!user) {
 		return null;
 	}
 
-	return (
-		<ul>
-			<li>
-				<strong>User Id</strong> {userId}
-			</li>
-			<li>
-				<strong>Username</strong> {user.username}
-			</li>
-			<li>
-				<strong>Email</strong> {user.email}
-			</li>
-			<li>
-				<img src={user.prof_pic_url}/>
+	if (!sessionUser) return <Redirect to="/login" />;
 
-			</li>
-		</ul>
+	return (
+		<div className="profileWholeContainer">
+			<div className="info-images">
+				<div className="Topprof">
+					<div className="Profpiccontainer">
+						<img
+							className="profPic"
+							src={user.prof_pic_url}
+							alt="profile pic"
+						/>
+					</div>
+					<div className="allinfo">
+						<div className="userandedit">
+							<p className="profUsername">{user?.username}</p>
+							{/* {account.id === parseInt(userId) ? (
+								<div className="edit-prof-button">
+									<NavLink className="edit-prof" to="/accounts/edit">
+										Edit profile
+									</NavLink>
+								</div>
+							) : (
+								<></>
+							)} */}
+						</div>
+						<div className="prof-description-container">
+							<p className="profDescription">{user.description}</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="imgContainer">
+					{songs.map((song) => (
+						<div key={song.id}>
+							<NavLink exact to={`/images/${song.id}`}>
+								{parseInt(userId) === song.user_id ? (
+									<div className="profsongs">
+										<img
+											alt="uploaded"
+											className="profsong"
+											src={song.song_url}
+										/>
+									</div>
+								) : (
+									<></>
+								)}
+							</NavLink>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
 	);
 }
 export default User;
